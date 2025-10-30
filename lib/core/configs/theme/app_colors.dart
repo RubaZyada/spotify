8a3812135a5290040bae66babef5ee8a6407a7df
RubaZyada <robazyada167@gmail.com>
2025-10-30 21:10:33 +0200

    first commit

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff42C83C);
+  static const lightBackground = Color(0xffF2F2F2);
+  static const darkBackground = Color(0xff0D0C0C);
+  static const greyText = Color(0xffBEBEBE);
+   static const greyBackground = Color(0xff343434);
+}
---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_app/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
+     fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor,
+        textStyle: const TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+   
+  );
+  static final darkTheme = ThemeData(
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark ,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor,
+        textStyle: const TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+   
+  );
+}
