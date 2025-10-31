4ad52a09cf6541f79aac0360104e9892a7194be1
RubaZyada <robazyada167@gmail.com>
2025-10-31 21:51:35 +0200

    theme cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_app/core/configs/theme/app_theme.dart';
-import 'package:spotify_app/presentation/choose_mode/screens/choose_mode.dart';
-//import 'package:spotify_app/presentation/intro/screens/get_started.dart';
-//import 'package:spotify_app/presentation/splash/screens/splash.dart';
+import 'package:spotify_app/presentation/choose_mode/bloc/theme_cubit.dart';
+import 'package:spotify_app/presentation/splash/screens/splash.dart';
 
 Future<void> main() async {
   //تهيئة الفلاتر قبل تشغيل التطبيق.
   WidgetsFlutterBinding.ensureInitialized();
   //تحدد مكان حفظ البيانات.
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: kIsWeb//يتحقق هل التطبيق يعمل على الويب أو الموبايل/ديسكتوب.
-        ? HydratedStorageDirectory.web//يعني احفظ الحالة داخل localStorage الخاص بالمتصفح.
-        : HydratedStorageDirectory((await getTemporaryDirectory()).path),//يعني احفظ الحالة في مجلد مؤقت على الجهاز.
+    storageDirectory:
+        kIsWeb //يتحقق هل التطبيق يعمل على الويب أو الموبايل/ديسكتوب.
+        ? HydratedStorageDirectory
+              .web //يعني احفظ الحالة داخل localStorage الخاص بالمتصفح.
+        : HydratedStorageDirectory(
+            (await getTemporaryDirectory()).path,
+          ), //يعني احفظ الحالة في مجلد مؤقت على الجهاز.
   );
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -24,11 +28,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,
-      home: ChooseModeScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => ThemeCubit()),
+        // BlocProvider(
+        //   create: (context) => SubjectBloc(),
+        // ),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, mode) =>
+          MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: mode,
+            home: SplashScreen(),
+          )
+        ,
+      ),
     );
   }
 }
