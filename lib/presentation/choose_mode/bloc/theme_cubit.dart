6c4638f9f2bb8d58ff2c581ea2d3502ddf111a0d
RubaZyada <robazyada167@gmail.com>
2025-10-31 22:35:18 +0200

    theme

--- lib/presentation/choose_mode/bloc/theme_cubit.dart
+++ lib/presentation/choose_mode/bloc/theme_cubit.dart
@@ -10,20 +10,12 @@ class ThemeCubit extends HydratedCubit<ThemeMode> {
   // تحويل الحالة إلى JSON وتخزينها
   @override
   Map<String, dynamic>? toJson(ThemeMode state) {
-    return {'theme': state.toString().split('.').last};
+    return {'theme': state.index};
   }
 
   // استرجاع الحالة عند فتح التطبيق مجددًا
   @override
   ThemeMode? fromJson(Map<String, dynamic> json) {
-    final themeString = json['theme'] as String?;
-    switch (themeString) {
-      case 'light':
-        return ThemeMode.light;
-      case 'dark':
-        return ThemeMode.dark;
-      default:
-        return ThemeMode.system;
-    }
+    return ThemeMode.values[json['theme']  as int];
   }
 }
