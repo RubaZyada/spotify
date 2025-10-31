1dc2edbb6e2ba1d3a903520739fb197964b13bec
RubaZyada <robazyada167@gmail.com>
2025-10-31 20:15:11 +0200

    Initialize hydratedbloc in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:spotify_app/core/configs/theme/app_theme.dart';
 import 'package:spotify_app/presentation/choose_mode/screens/choose_mode.dart';
-import 'package:spotify_app/presentation/intro/screens/get_started.dart';
-import 'package:spotify_app/presentation/splash/screens/splash.dart';
+//import 'package:spotify_app/presentation/intro/screens/get_started.dart';
+//import 'package:spotify_app/presentation/splash/screens/splash.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  //هذا السطر يربط Flutter بالنظام الأساسي (platform) قبل تشغيل التطبيق.
+  WidgetsFlutterBinding.ensureInitialized();
+  //تحدد مكان حفظ البيانات.
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb//يتحقق هل التطبيق يعمل على الويب أو الموبايل/ديسكتوب.
+        ? HydratedStorageDirectory.web//يعني احفظ الحالة داخل localStorage الخاص بالمتصفح.
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),//يعني احفظ الحالة في مجلد مؤقت على الجهاز.
+  );
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
